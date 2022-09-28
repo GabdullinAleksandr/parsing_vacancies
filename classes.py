c982e941e7ca22a05d79b79377918ec13918c760
Gabdullin_Alex <alexrexp@gmail.com>
2022-09-28 11:22:50 +0300

    change 4

--- classes.py
+++ classes.py
@@ -10,20 +10,32 @@ class Engine(ABC):
 
 
 class HH(Engine):
-    def __init__(self, name):
+    def __init__(self, name, number_vac):
         self.name = name
+        self.number_vac = number_vac
 
     def get_request(self):
-        with open('vacancy.txt', 'a', encoding='utf-8') as file:
-            for i in range(5):
-                par = {'text': self.name, 'only_with_salary': True,
-                       'per_page': '10', 'page': i, 'area': '113'}
-                response = requests.get('https://api.hh.ru/vacancies/', params=par).json()
-                for j in range(10):
-                    dict_vacancy = response['items'][j]
-                    cl_vacancy = Vacancy(dict_vacancy)
-                    file.write('\n' + cl_vacancy.__repr__() + '---')
-
+        counter = 1
+        try:
+            with open('vacancy.txt', 'a', encoding='utf-8') as file:
+                for i in range(self.number_vac // 10):
+                    print(f'Обработка страницы №{i+1}')
+                    par = {'text': self.name, 'only_with_salary': True,
+                           'per_page': '10', 'page': i, 'area': '113'}
+                    response = requests.get('https://api.hh.ru/vacancies/', params=par).json()
+                    for j in range(10):
+                        try:
+                            dict_vacancy = response['items'][j]
+                        except KeyError:
+                            print('KeyError')
+                            continue
+                        cl_vacancy = Vacancy(dict_vacancy)
+                        file.write(str(counter) + '***' + cl_vacancy.__repr__() + '\n')
+                        counter += 1
+        except IndexError:
+            print('Найдены все доступные вакансии')
+            return counter
+        return counter
 class Superjob(Engine):
     def get_request(self):
         pass
@@ -47,8 +59,8 @@ class Vacancy():
                f"{self.dict_vacancy['salary']['currency']}"
 
     def __repr__(self):
-        return f'Название вакансии - {self._title_job}\n' \
-               f'Зарплата - {self._salary_job}\n' \
-               f'Ссылка - {self._link_job}\n' \
-               f'Описание: {self._description_job}\n'
+        return f'Название вакансии - {self._title_job}--' \
+               f'Зарплата - {self._salary_job}--' \
+               f'Ссылка - {self._link_job}--' \
+               f'Описание: {self._description_job}--'
 
--- main.py
+++ main.py
@@ -1,24 +1,66 @@
 from utlis import *
 from classes import *
 import json
-
-# par = {'text': 'python', 'only_with_salary': True, 'per_page': 10, 'page': 1}
-# response = requests.get('https://api.hh.ru/vacancies/', params=par).json()
-# with open('', 'w', encoding='utf-8') as file_jobs:
-#     json.dump(response.json(), file_jobs, indent=4, ensure_ascii=False)
-
-
-
+from random import randint
+def check_us_inp(user, number_of_vac):
+    check = user.isdigit()
+    if check == False:
+        return False
+    elif user == '1':
+        get_top_five()
+    elif user == '2':
+        get_random_vac(number_of_vac)
+    elif user == '3':
+        get_ten_first()
+    elif user == '4':
+        get_ten_if()
+    else:
+        return False
+def get_top_five():
+    pass
+def get_random_vac(counter):
+    random_num = randint(1,counter)
+    with open('vacancy.txt', 'r', encoding='utf-8') as f:
+        res = list(filter(lambda line: f'{str(random_num)}***' in line, f))
+        res = ''.join(res).replace('--','\n').replace('***','\n')
+        print(res)
+def get_ten_first():
+    with open('vacancy.txt', 'r', encoding='utf-8') as f:
+        res = list(filter(lambda line: f'{str(random_num)}***' in line, f))
+        res = ''.join(res).replace('--', '\n').replace('***', '\n')
+        print(res)
+    pass
+def get_ten_if():
+    pass
 
 def main():
-    # with open('vacancy.txt', 'wb') as f:
-    #     pass
-
-
+    with open('vacancy.txt', 'wb') as f:
+        pass
     request_user = input('Введите ключевое слово для поиска профессии - ')
-    head_h = HH(request_user)
-    head_h.get_request()
-
+    try:
+        number_of_vac_user = int(input('Введите требуемое кол-во вакансий(не менее 10) - '))
+    except:
+        print('incorrect input')
+        quit()
+    head_h = HH(request_user, number_of_vac_user)
+    print('Выполняется поиск по HH')
+    number_of_vac = head_h.get_request() - 1
+    if number_of_vac == 0:
+        print('Вакансий не найдено')
+        quit()
+    while True:
+        print(f'Найдено вакансий: {number_of_vac}')
+        print('Напишите номер пункта для вызвова или "stop":\n'
+              '1 - получить топ пять вакансий по зарплате\n'
+              '2 - получить случайную вакансию\n'
+              '3 - получить 10 первых вакансий\n'
+              '4 - получить 10 вакансий с зарплатой выше 300000 руб.\n')
+        user_input = input()
+        if user_input == 'stop':
+            break
+        if check_us_inp(user_input, number_of_vac) == False:
+            print('incorrect input\nПопробуйте еще раз')
+            continue
 
 if __name__ == '__main__':
     main()
